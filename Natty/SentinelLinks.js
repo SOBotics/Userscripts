50f0f23e89c1a637913e90e393e864d1ead5219c
Bhargav Rao <bhargav.rao448@gmail.com>
2017-10-14 11:34:48 -0700

    Update SentinelLinks.js

--- Natty/SentinelLinks.js
+++ Natty/SentinelLinks.js
@@ -12,8 +12,8 @@
 
 (function(){
   $(".post-menu").each ( function (index) {
-      var thisObj = $(this)
-    var postLink  = thisObj.find('a[class="short-link"]').attr("href");
+      var thisObj = $(this);
+      var postLink  = thisObj.find('a[class="short-link"]').attr("href");
       if(postLink && postLink.indexOf('a')>=0){
       thisObj.append('<span class="lsep">|</span><a href="http://sentinel.erwaysoftware.com/posts/aid/' + postLink.split("/")[2]  +'">sentinel</a>');
     }
