ea409f02cff0b765a783902faffe394bc8532264
Tobias Olbrich <public.tobias.olbrich@gmail.com>
2017-11-21 01:32:18 +0100

    Missing semicolon

--- Natty/NattyReporter.user.js
+++ Natty/NattyReporter.user.js
@@ -252,7 +252,7 @@ const ScriptToInject = function() {
   $(document).ready(function() {
     handleAnswers(); 
   });
-}
+};
 
 const ScriptToInjectNode = document.createElement('script');
 document.body.appendChild(ScriptToInjectNode);
